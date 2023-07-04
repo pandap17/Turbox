4077c73ae1692602d47f31fe5bfc746a220d26ba
pandap17 <135696214+pandap17@users.noreply.github.com>
2023-07-04 17:52:55 -0500

    Update screenres.js

--- dependencies/screenres.js
+++ dependencies/screenres.js
@@ -1,30 +1,41 @@
-class ScreenResolutionExtension {
-    getInfo() {
-        return {
-            id: 'screenResolution',
-            name: 'Screen Resolution',
-            blocks: [
-                {
-                    opcode: 'getScreenWidth',
-                    blockType: 'reporter',
-                    text: 'screen width'
-                },
-                {
-                    opcode: 'getScreenHeight',
-                    blockType: 'reporter',
-                    text: 'screen height'
-                }
-            ]
-        };
+(function(Scratch) {
+  'use strict';
+  
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('Screen resolution extension must be run unsandboxed');
+  }
+  
+  class ScreenResolution {
+    getInfo () {
+      return {
+        id: 'screenResolution',
+        name: 'Screen Resolution',
+        color1: '#4a90d9',
+        color2: '#4280d7',
+        color3: '#3871d6',
+        blocks: [
+          {
+            opcode: 'getScreenWidth',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'screen width'
+          },
+          {
+            opcode: 'getScreenHeight',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'screen height'
+          }
+        ]
+      };
     }
-
-    getScreenWidth() {
-        return window.screen.width;
+    
+    getScreenWidth () {
+      return window.innerWidth;
     }
-
-    getScreenHeight() {
-        return window.screen.height;
+    
+    getScreenHeight () {
+      return window.innerHeight;
     }
-}
-
-TurboWarp.addExtension(new ScreenResolutionExtension());
+  }
+  
+  Scratch.extensions.register(new ScreenResolution());
+})(Scratch);
